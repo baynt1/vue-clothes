b10393032d0f090425819c61268e05abc21e54cc
Emilia <81705094+isemilia@users.noreply.github.com>
2024-06-09 16:41:19 +0500

    feat: container

---
+++ src/app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <Container>
+    <div>Hello</div>
+  </Container>
+</template>
+
+<script setup lang="ts">
+import Container from "../components/container.vue";
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './shared/assets/styles/index.scss'
-import App from './app.vue'
+import App from './app/app.vue'
 
 createApp(App).mount('#app')
